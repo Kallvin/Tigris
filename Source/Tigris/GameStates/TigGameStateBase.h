217eee173f276818dd4b47cf4e8813aa69710f8a
Kallvin <iswkallvin@gmail.com>
2021-01-17 22:43:55 -0800

    New TigGameStateBase
    Deleted Original TigGameStateBase.
    Generated project files.
    Rebuilt Tigris Module.
    Built project from VS.
    Validated data.
    Recreated TigGameStateBase.
    Built Project from VS.
    
    -Kal

---
+++ Source/Tigris/GameStates/TigGameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "TigGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIGRIS_API ATigGameStateBase : public AGameState
+{
+	GENERATED_BODY()
+	
+};
